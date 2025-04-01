f41aea23d9a6eaa239b30def1039634536045a63
Olewwwka <netvinkakk@gmail.com>
2025-04-01 22:41:53 +0300

    add: user repository and unit of work

---
+++ Lib.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Lib.Core.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public LibraryDbContext _dbContext;
+        public IUsersRepository UsersRepository { get; }
+
+        public UnitOfWork(LibraryDbContext dbcontext)
+        {
+            _dbContext = dbcontext;
+            UsersRepository = new UsersRepository(_dbContext);
+        }
+    }
+}
---
+++ Lib.Infrastructure/Repositories/UsersRepository.cs
@@ -0,0 +1,37 @@
+﻿using Lib.Core.Abstractions;
+using Lib.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Infrastructure.Repositories
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly LibraryDbContext _context;
+        public UsersRepository(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserEntity> GetUserByIdAsync(Guid id)
+        {
+            var user = await _context.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Id == id);
+            return user;
+        }
+
+        public async Task<UserEntity> GetUserByEmailAsync(string email)
+        {
+            var user = await _context.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Email == email);
+            return user;
+        }
+        public async Task AddUserAsync(UserEntity user)
+        {
+            await _context.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}

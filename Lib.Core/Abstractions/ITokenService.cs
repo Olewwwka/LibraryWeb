0c1135e35cbfbdb74eff7fff881cee1b090e7149
Olewwwka <netvinkakk@gmail.com>
2025-04-01 22:42:10 +0300

    add: abstractions

---
+++ Lib.Core/Abstractions/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace Lib.Core.Abstractions
+{
+    public interface ITokenService
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+        Task StoreRefreshTokenAsync(Guid userId, string refreshToken);
+        Task<bool> ValidateRefreshTokenAsync(string userId, string refreshToken);
+    }
+}
---
+++ Lib.Core/Abstractions/IUsersRepository.cs
@@ -0,0 +1,16 @@
+﻿using Lib.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Core.Abstractions
+{
+    public interface IUsersRepository
+    {
+        Task<UserEntity> GetUserByEmailAsync(string email);
+        Task<UserEntity> GetUserByIdAsync(Guid id);
+        Task AddUserAsync(UserEntity user);
+    }
+}

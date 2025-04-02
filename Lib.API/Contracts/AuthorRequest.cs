6b624f10c6c9d29c5146b536246bae5cbbc23973
Olewwwka <netvinkakk@gmail.com>
2025-04-02 20:35:22 +0300

    add: contracts

---
+++ Lib.API/Contracts/AuthorRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lib.API.Contracts
+{
+    public record AuthorRequest(
+        [Required] string Name,
+        [Required] string Surname,
+        [Required] string Country,
+        [Required] DateTime Birthday
+        );
+}

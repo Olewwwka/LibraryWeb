0ac933ed3e797e8163ccf9d99c6b668a4b5c65f4
Olewwwka <netvinkakk@gmail.com>
2025-04-03 17:33:57 +0300

    add: extension for useCases

---
+++ Lib.API/Extensions/AddUseCasesExtention.cs
@@ -0,0 +1,20 @@
+﻿using Lib.Application.UseCases.Books;
+
+namespace Lib.API.Extensions
+{
+    public static class AddUseCasesExtention
+    {
+        public static void AddUseCases(this IServiceCollection services)
+        {
+            var assembly = typeof(AddBookUseCase).Assembly;
+
+            var useCasesTypes = assembly.GetTypes()
+                .Where(t => t.Name.EndsWith("UseCase") && t.IsClass && !t.IsAbstract);
+
+            foreach (var type in useCasesTypes)
+            {
+                services.AddScoped(type);
+            }
+        }
+    }
+}

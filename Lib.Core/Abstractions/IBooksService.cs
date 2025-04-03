81fedbba70a7502e72c07885ac33a4b776a375a8
Olewwwka <netvinkakk@gmail.com>
2025-04-03 14:01:13 +0300

    update: booksService

--- Lib.Core/Abstractions/IBooksService.cs
+++ Lib.Core/Abstractions/IBooksService.cs
@@ -13,5 +13,8 @@ namespace Lib.Core.Abstractions
         Task<Book> AddBook(string isbn, string name, Genre genre, string description, Guid authorId, CancellationToken cancellationToken);
         Task<List<Book>> GetAllBooks(CancellationToken cancellationToken);
         Task<Book> GetBookById(Guid id, CancellationToken cancellationToken);
+        Task<Book> GetBookByISBN(string isbn, CancellationToken cancellationToken);
+        Task<Guid> UpdateBookInfo(Guid id, string isbn, string name, Genre genre, string description, CancellationToken cancellationToken);
+        Task<Guid> DeleteBook(Guid id, CancellationToken cancellationToken);
     }
 }
